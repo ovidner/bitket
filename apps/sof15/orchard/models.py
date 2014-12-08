efd9dd309f54b041a6e487ce9f0e37893c343914
Olle Vidner <olle@vidner.se>
2014-12-08 22:25:02 +0100

    Model updated for better consistency and for needed features

--- apps/sof15/orchard/models.py
+++ apps/sof15/orchard/models.py
@@ -8,10 +8,12 @@ from django.utils.encoding import python_2_unicode_compatible
 class Orchestra(models.Model):
     name = models.CharField(max_length=256)
 
+    members = models.ManyToManyField('OrchestraMember', related_name='orchestras', through='OrchestraMembership')
+
     class Meta:
         permissions = (
             ('approve_members', _('Approve members')),
-            ('approve_invoicing', _('Approve members for invoicing')),
+            ('approve_members_for_invoicing', _('Approve members for invoicing')),
         )
 
     def __str__(self):
@@ -20,11 +22,14 @@ class Orchestra(models.Model):
 
 @python_2_unicode_compatible
 class OrchestraMembership(models.Model):
-    orchestra = models.ForeignKey('Orchestra')
-    member = models.ForeignKey('OrchestraMember')
+    orchestra = models.ForeignKey('Orchestra', related_name='memberships')
+    member = models.ForeignKey('OrchestraMember', related_name='memberships')
 
-    primary = models.BooleanField(default=False)
     active = models.BooleanField(default=False)
+    primary = models.BooleanField(default=False)
+
+    approved = models.NullBooleanField()
+    approved_for_invoicing = models.NullBooleanField()
 
     def __str__(self):
         return '{0}: {1}'.format(self.orchestra, self.person)
@@ -34,10 +39,8 @@ class OrchestraMembership(models.Model):
 class OrchestraMember(models.Model):
     person = models.OneToOneField('tickle.Person', related_name='orchestra_member')
 
-    orchestras = models.ManyToManyField('Orchestra', through='OrchestraMembership')
-
     def __str__(self):
-        return '{0} ({1})'.format(self.person.full_name, self.orchestra.name)
+        return self.person.full_name
 
 
 @python_2_unicode_compatible
