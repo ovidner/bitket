24ca7ff42534e81bfcfddca52c6f7dbbde4c8c6a
Olle Vidner <olle@vidner.se>
2014-12-22 01:13:32 +0100

    Karthago URLs included

--- apps/sof15/sof15/urls.py
+++ apps/sof15/sof15/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', TemplateView.as_view(template_name='base.html'))
+    url(r'^$', TemplateView.as_view(template_name='base.html')),
+    url(r'^kartege/', include('karthago.urls')),
 )
