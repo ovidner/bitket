8680171d715b733402337e902fd3ff93b8944660
Gustav Häger <hager.gustav@gmail.com>
2015-01-18 17:45:53 +0100

    Added models

--- invar/admin.py
+++ invar/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from Invar.models import Invoice, InvoiceRow
 
 # Register your models here.
+
+@admin.register(Invoice)
+class InvoiceAdmin(admin.modelAdmin):
+    pass
--- invar/models.py
+++ invar/models.py
@@ -3,24 +3,16 @@ from django.db import models
 
 
 class Invoice(models.Model):
-    pass
+    invoice_number = models.IntegerField()
+    customerFirstname = models.CharField(max_length=255)
+    customerLastname = models.CharField(max_length=255)
+    customerOrganization = models.CharField(max_length=255, default='')
+    customerPNR = models.CharField(max_length=10)
+    customerEmail = models.CharField(max_length=255)
 
 
 class InvoiceRow(models.Model):
     invoice = models.ForeignKey('Invoice')
-    amount = models.DecimalField(max_digits=9, decimal_places=2)
-
-
-class ProductInvoiceRow(InvoiceRow):
-    product = models.ForeignKey('tickle.Product')
-
-
-class OtherInvoiceRow(InvoiceRow):
-    text = models.CharField(max_length=256)
-
-
-class Transaction(models.Model):
-    time = models.DateTimeField()
-    invoice = models.ForeignKey('Invoice', null=True, blank=True)
-    reference = models.CharField(max_length=512)
-    amount = models.DecimalField(max_digits=9, decimal_places=2)
+    itemCost = models.DecimalField(max_digits=9, decimal_places=2)
+    nrItems = models.IntegerField(default=1)
+    itemName = models.CharField(max_length=255)
