3192530cdc4be0690e10ccfae7c8a46773d5b72a
Olle Vidner <olle@vidner.se>
2015-02-09 00:56:59 +0100

    Closed Kårtege registration

--- karthago/urls.py
+++ karthago/urls.py
@@ -6,6 +6,7 @@ from .views import EntryCreate
 
 urlpatterns = patterns(
     '',
-    url(r'^entries/add/$', EntryCreate.as_view(), name='create_kartege_entry'),
+    # url(r'^entries/add/$', EntryCreate.as_view(), name='create_kartege_entry'),
+    url(r'^entries/add/$', TemplateView.as_view(template_name='karthago/entries/create_closed.html'), name='create_kartege_entry'),
     url(r'^entries/add/success/$', TemplateView.as_view(template_name='karthago/entries/create_success.html'), name='create_kartege_entry_success'),
 )
