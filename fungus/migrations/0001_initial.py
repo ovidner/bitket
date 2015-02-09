d662599c7e220c0f11ff6387ff800b1d86ca9767
Olle Vidner <olle@vidner.se>
2015-02-09 17:00:39 +0100

    Un-reverted migration :))

--- fungus/migrations/0001_initial.py
+++ fungus/migrations/0001_initial.py
@@ -8,20 +8,38 @@ import mptt.fields
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('tickle', '0007_auto_20150126_1652'),
+        ('tickle', '0008_auto_20150202_0003'),
     ]
 
     operations = [
+        migrations.CreateModel(
+            name='Location',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=256)),
+                ('lft', models.PositiveIntegerField(editable=False, db_index=True)),
+                ('rght', models.PositiveIntegerField(editable=False, db_index=True)),
+                ('tree_id', models.PositiveIntegerField(editable=False, db_index=True)),
+                ('level', models.PositiveIntegerField(editable=False, db_index=True)),
+                ('parent', mptt.fields.TreeForeignKey(related_name='children', blank=True, to='fungus.Location', null=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
         migrations.CreateModel(
             name='Shift',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('start', models.DateTimeField()),
                 ('end', models.DateTimeField()),
-                ('min_workers', models.PositiveIntegerField(default=1, help_text='The number of workers needed on this shift for critical operation.', verbose_name='minimum number of workers')),
-                ('max_workers', models.PositiveIntegerField(verbose_name='maximum number of workers')),
+                ('people_max', models.PositiveIntegerField(null=True, verbose_name='maximum number of workers', blank=True)),
+                ('people_warning', models.PositiveIntegerField(null=True, verbose_name='warning number of workers', blank=True)),
+                ('people_critical', models.PositiveIntegerField(help_text='The number of workers needed on this shift for critical operation.', null=True, verbose_name='critical number of workers', blank=True)),
                 ('public', models.BooleanField(default=True, help_text="If unchecked, this shift won't be visible or available for registration by public users.", verbose_name='public')),
-                ('responsible', models.ForeignKey(related_name='shift_responsibilities', to='tickle.Person')),
+                ('location', models.ForeignKey(related_name='shifts', blank=True, to='fungus.Location', null=True)),
+                ('responsible', models.ForeignKey(related_name='shift_responsibilities', blank=True, to='tickle.Person', null=True)),
             ],
             options={
             },
@@ -31,7 +49,8 @@ class Migration(migrations.Migration):
             name='ShiftRegistration',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('shift', models.ForeignKey(to='fungus.Shift')),
+                ('person', models.ForeignKey(related_name='shift_registrations', to='tickle.Person')),
+                ('shift', models.ForeignKey(related_name='registrations', to='fungus.Shift')),
             ],
             options={
             },
@@ -57,18 +76,12 @@ class Migration(migrations.Migration):
             name='Worker',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('ice_number', models.CharField(max_length=16, verbose_name='ICE number')),
+                ('ice_number', models.CharField(max_length=16, null=True, verbose_name='ICE number', blank=True)),
                 ('person', models.OneToOneField(related_name='worker', to='tickle.Person')),
             ],
             options={
             },
             bases=(models.Model,),
-        ),
-        migrations.AddField(
-            model_name='shiftregistration',
-            name='worker',
-            field=models.ForeignKey(to='fungus.Worker'),
-            preserve_default=True,
         ),
         migrations.AddField(
             model_name='shift',
