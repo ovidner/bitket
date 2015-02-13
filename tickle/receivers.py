609c47138f896b5819718e32e5d1da24ec237ed7
Olle Vidner <olle@vidner.se>
2015-02-13 10:46:55 +0100

    Excluded ANONYMOUS_USER_ID

--- tickle/receivers.py
+++ tickle/receivers.py
@@ -4,7 +4,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 from django.dispatch import receiver
-
+from django.conf import settings
 from django_auth_ldap.backend import populate_user
 from django.db.models.signals import pre_save, post_save
 
@@ -21,7 +21,7 @@ def populate_liu_id(sender, **kwargs):
 
 @receiver(pre_save, sender=TickleUser)
 def generate_and_send_password_user(sender, instance, **kwargs):
-    if not instance.password:
+    if not instance.password and instance.id != settings.ANONYMOUS_USER_ID:
         instance.generate_and_send_password()
 
 
