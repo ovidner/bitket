53807ce812d064e1f038b1a0a853a1166f4c090e
Olle Vidner <olle.vidner@sof15.se>
2015-02-23 02:00:50 +0100

    Integrated LiU ids in Tickle and fixed migrations

--- tickle/models/people.py
+++ tickle/models/people.py
@@ -7,7 +7,78 @@ from django.utils.translation import ugettext_lazy as _
 from django.utils.encoding import python_2_unicode_compatible
 from django.conf import settings
 
+from liu.kobra import KOBRAClient
+
 from tickle.utils.mail import TemplatedEmail
+
+
+@python_2_unicode_compatible
+class StudentUnion(models.Model):
+    name = models.CharField(max_length=256, verbose_name=_('name'))
+
+    def __str__(self):
+        return self.name
+
+
+class LiUIDQuerySet(models.QuerySet):
+    def fetch(self):
+        for i in self:
+            i.fetch()
+
+
+@python_2_unicode_compatible
+class LiUID(models.Model):
+    """
+    Stores a LiU ID. Fetches information from KOBRA with the fetch() method.
+
+    liu_id is the only mandatory field.
+    """
+    liu_id = models.CharField(max_length=10, verbose_name=_('LiU ID'))
+
+    first_name = models.CharField(max_length=256, blank=True, verbose_name=_('first name'))
+    last_name = models.CharField(max_length=256, blank=True, verbose_name=_('last name'))
+
+    pid = models.CharField(max_length=11, blank=True, verbose_name=_('national identity number'))
+
+    barcode_number = models.CharField(max_length=32, blank=True, verbose_name=_('magnet/barcode card number'))
+    rfid_number = models.CharField(max_length=32, blank=True, verbose_name=_('RFID card number'))
+
+    student_union = models.ForeignKey('StudentUnion', related_name='members', blank=True, null=True,
+                                      verbose_name=_('student union'))
+
+    blocked = models.NullBooleanField(verbose_name=_('blocked'))
+
+    objects = LiUIDQuerySet.as_manager()
+
+    class Meta:
+        verbose_name = _('LiU ID')
+        verbose_name_plural = _('LiU IDs')
+
+    def __str__(self):
+        return self.liu_id
+
+    def fetch(self):
+        """
+        Updates the fields using KOBRA. Uses LiU ID to find user.
+        """
+        client = KOBRAClient(settings.LIU_KOBRA_USER, settings.LIU_KOBRA_API_KEY)
+        # We don't catch any exceptions here. You might want to catch them in your own applications instead.
+        data = client.get_student(liu_id=self.liu_id)
+
+        if data:
+            self.first_name = data['first_name']
+            self.last_name = data['last_name']
+
+            self.id_number = data['personal_number']
+
+            self.barcode_number = data['barcode_number']
+            self.rfid_number = data['rfid_number']
+
+            self.student_union = StudentUnion.objects.get_or_create(name=data['union'])[0]
+
+            self.blocked = data['blocked']
+
+            self.save()
 
 
 @python_2_unicode_compatible
@@ -23,7 +94,7 @@ class Person(models.Model):
         verbose_name=_('national identity code'),
         help_text=_('Last 4 digits in Swedish national identity number.'),
     )
-    liu_id = models.OneToOneField('liu.LiUID', blank=True, null=True, verbose_name=_('LiU ID'))
+    liu_id = models.OneToOneField('LiUID', blank=True, null=True, verbose_name=_('LiU ID'))
 
     phone = models.CharField(max_length=24, blank=True, verbose_name=('phone number'))
     email = models.EmailField(max_length=256, unique=True, verbose_name=_('email address'))
