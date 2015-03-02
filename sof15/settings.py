7ef311a1656e0971b085e4d1e282a02fdd983aa5
Olle Vidner <olle.vidner@sof15.se>
2015-03-02 14:35:40 +0100

    Changed default auth and perm classes

--- sof15/settings.py
+++ sof15/settings.py
@@ -80,11 +80,13 @@ MIDDLEWARE_CLASSES = (
 )
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.DjangoObjectPermissions',
+    ),
 }
 
 # Database backed cache backend.
