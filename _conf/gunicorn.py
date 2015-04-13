474243207cc2ccc915b2e43fbeabdf531419caa7
Olle Vidner <olle.vidner@sof15.se>
2015-04-13 19:56:03 +0200

    Gunicorn parameters configurable

--- _conf/gunicorn.py
+++ _conf/gunicorn.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 import multiprocessing
+from os import getenv
 
 pythonpath = '/home/sof15/app'
 bind = '0.0.0.0:8080'
 
-workers = multiprocessing.cpu_count() * 2 + 1  # todo: optimize
-threads = 1  # todo: optimize
+workers = int(getenv('GUNICORN_WORKERS', multiprocessing.cpu_count()))
+threads = int(getenv('GUNICORN_THREADS', 1))
 
 user = None
-loglevel = 'error'
+loglevel = getenv('GUNICORN_LOG_LEVEL', 'error')
 errorlog = '-'  # stderr
