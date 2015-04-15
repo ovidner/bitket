cb3e5a14814c444b57d7c6816f4f1fffe4b8f793
Olle Vidner <olle.vidner@sof15.se>
2015-04-16 00:27:36 +0200

    Added password reset urls

--- sof15/urls.py
+++ sof15/urls.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 
 from tickle.views.people import LoginView, ProfileView, ChangePasswordView
 from orchard.views import ApproveOrchestraMemberView, RegisterOrchestraMemberView
@@ -20,12 +22,18 @@ urlpatterns = patterns(
     url(r'^people/(?P<pk>\d+)/$', ProfileView.as_view(), name='profile'),
     url(r'^people/me/$', LoginView.as_view(), name='profile_me'),
 
-    url(r'^admin/fungus/shiftregistration/shifchange/$', ShiftChangeView.as_view(), name='change_selected_shifts'),
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^karthago/', include('karthago.urls', namespace='karthago', app_name='karthago')),
     url(r'^orchard/', include('orchard.urls', namespace='orchard', app_name='orchard')),
     url(r'^fungus/', include('fungus.urls', namespace='fungus', app_name='fungus')),
 
     url(r'^api/', include('api.urls', namespace='api')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+    url(r'^admin/fungus/shiftregistration/shifchange/$', ShiftChangeView.as_view(), name='change_selected_shifts'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/password_reset/$', auth_views.password_reset, name='admin_password_reset'),
+    url(r'^admin/password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', auth_views.password_reset_confirm,
+        name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 )
