7c5302fbd871a0ced15b73bc9a2858fd27e94d14
Olle Vidner <olle.vidner@sof15.se>
2015-04-20 14:06:29 +0200

    Removed implicit creation of passwords

--- tickle/receivers.py
+++ tickle/receivers.py
@@ -11,20 +11,6 @@ from django.contrib.auth.signals import user_logged_in
 from .models import TickleUser, Person, Discount, Holding, ShoppingCart
 
 
-@receiver(pre_save, sender=TickleUser)
-def generate_and_send_password_user(sender, instance, **kwargs):
-    # Do this only for non-LiU id users.
-    if not instance.person.liu_id and not instance.password and instance.id != settings.ANONYMOUS_USER_ID:
-        instance.generate_and_send_password()
-
-
-@receiver(pre_save, sender=Person)
-def generate_and_send_password_person(sender, instance, **kwargs):
-    # Do this only for non-LiU id users.
-    if not instance.liu_id and hasattr(instance, 'user') and not instance.user.password and instance.id != settings.ANONYMOUS_USER_ID:
-        instance.user.generate_and_send_password()
-
-
 @receiver(user_logged_in)
 def map_discount_eligibilities(sender, **kwargs):
     person = kwargs['user'].person
@@ -39,6 +25,5 @@ def create_shopping_cart(sender, **kwargs):
 
 @receiver(post_save, sender=Holding)
 def send_ticket(sender, instance, **kwargs):
-    print('hej')
     if instance.product.is_ticket_type and getattr(instance, 'purchase'):
         instance.send_ticket()
