4cd6d61392d6513af7df9ebbc78a62b4f0e3923c
Gustav Häger <hager.gustav@gmail.com>
2015-04-21 19:39:55 +0200

    Nu med mindre broken

--- invar/models.py
+++ invar/models.py
@@ -8,19 +8,6 @@ from enumfields import EnumField
 from enum import Enum
 import tickle.utils.mail as mail
 
-
-def get_new_invoice_id():
-    return randint(0, 1000000)
-
-
-def default_invoice_creation_date():
-    return now()
-
-
-def default_invoice_due_date():
-    return now() + datetime.timedelta(days=14)
-
-
 class Invoice(models.Model):
     class OrderStatus(Enum):
         GENERATED = 'g'
@@ -30,19 +17,21 @@ class Invoice(models.Model):
         OBLITERATED = 'o'
         LATE = 'L'
 
-    invoice_number = models.IntegerField(unique=True, verbose_name='Fakturanummer')
-    customerName = models.CharField(max_length=255, verbose_name='Namn', default='')
-    customerOrganization = models.CharField(max_length=255, default='', verbose_name='Förening')
-    customerPNR = models.CharField(max_length=10, verbose_name='Personnummer')
-    customerEmail = models.EmailField(max_length=254, verbose_name='mail')
-    create_date = models.DateField(auto_now_add=True, default=datetime.date.today())
+    invoice_number = models.IntegerField(unique=True, verbose_name='Fakturanummer', null=True)
+    customer_name = models.CharField(max_length=255, verbose_name='Namn', default='')
+    customer_organization = models.CharField(max_length=255, default='', verbose_name='Förening')
+    customer_PNR = models.CharField(max_length=10, verbose_name='Personnummer')
+    customer_mail = models.EmailField(max_length=254, verbose_name='mail')
+    create_date = models.DateField(auto_now_add=True, default=django.utils.timezone.now())
     sent_date = models.DateField(null=True)
-    due_date = models.DateField(default=datetime.date.today() + datetime.timedelta(days=21))
+    due_date = models.DateField(default=None, nullable=True)
     current_status = EnumField(OrderStatus, max_length=1, default='g')
+    invoice_ocr = models.CharField(max_length=255, unique=True, verbose_name='OCR')
 
     def send_invoice(self):
         self.sent_date = django.utils.timezone.now()
-        self.current_status = 'S'
+        self.due_date = django.utils.timezone.now() + django.utils.timezone.timedelta(days=14)
+        self.current_status = self.OrderStatus.SENT
         self.save()
         self.send_invoice_mail()
 
@@ -54,25 +43,34 @@ class Invoice(models.Model):
 
     def get_invoice_data(self):
         data = dict()
-        data['customerName'] = self.customerName
-        data['customerOrganization'] = self.customerOrganization
-        data['customerPNR'] = self.customerPNR
-        data['sentDate'] = self.sent_date
-        data['dueDate'] = self.due_date
+        data['customer_name'] = self.customer_name
+        data['customer_organization'] = self.customer_organization
+        data['customer_PNR'] = self.customerPNR
+        data['sent_date'] = self.sent_date
+        data['due_date'] = self.due_date
+        data['invoice_number'] = self.invoice_number
         #retrive the invoice rows
 
-        products_summary = dict()
         products_specification = list()
-
+        total_sum = 0
         for row in self.rows.all():
-            products_specification.append(row.get_details())
-            if row.itemName in products_summary:
-                products_summary[row.itemName] += row.nrItems
-            else:
-                products_summary[row.itemName] = row.nrItems
+            row_details = row.get_details()
+            row_details['row_total'] = row_details['item_cost'] * row_details['num_items']
+            products_specification.append(row_details)
+            total_sum = row_details['item_cost'] * row_details['num_items']
 
-    create_date = models.DateField(default=default_invoice_creation_date)
-    due_date = models.DateField(default=default_invoice_due_date)
+
+        data['products'] = products_specification
+        data['invoice_total'] = total_sum
+        return data
+
+    def get_luhn_check(self, invoice_id):
+        digits = str(invoice_id)
+        sum = 0
+        for number in digits:
+            sum = sum + int(number)
+
+        return str(sum*9)[-1]
 
 
 class InvoiceRow(models.Model):
@@ -85,27 +83,29 @@ class InvoiceRow(models.Model):
     def get_details(self):
         details = dict()
         if self.holdingBackref:
-            details['ownerId'] = self.holdingBackref.person.pk
-            details['ownerName'] = self.holdingBackref.person.full_name
+            details['owner_id'] = self.holdingBackref.person.pk
+            details['owner_name'] = self.holdingBackref.person.full_name
 
-        details['itemName'] = self.itemName
-        details['nItems'] = self.nrItems
-        details['itemCost'] = self.itemCost
+        details['item_name'] = self.itemName
+        details['num_items'] = self.nrItems
+        details['item_cost'] = self.itemCost
         return details
 
 
-
-
 def generate_invoice(name, email, orgName, id_nr, stuff):
+    invoice_offset = 100000
     bill = Invoice(customerName=name,
                    customerOrganization=orgName,
                    customerPNR=id_nr,
-                   customerEmail=email,
-                   invoice_number=get_new_invoice_id(),
+                   customerEmail=email
                    )
+    bill.save()
+    bill.invoice_number = bill.pk + invoice_offset
+    bill.invoice_ocr = str(bill.invoice_number) + bill.get_luhn_check(bill.invoice_number)
     bill.save()
 
     for thing in stuff:
         row = InvoiceRow(invoice=bill, itemName=thing[0].name, nrItems=thing[1],
                          itemCost=thing[0].price, holdingBackref=thing[2])
         row.save()
+
