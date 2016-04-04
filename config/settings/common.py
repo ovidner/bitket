414ea33e93f5a7b4c71deca20d8782ae221a1c43
Olle Vidner <olle@vidner.se>
2016-04-04 11:40:32 +0200

    Added missing Opbeat app

--- config/settings/common.py
+++ config/settings/common.py
@@ -59,6 +59,7 @@ THIRD_PARTY_APPS = (
     #'allauth',  # registration
     #'allauth.account',  # registration
     #'allauth.socialaccount',  # registration
+    'opbeat.contrib.django',
 )
 
 # Apps specific for this project go here.
