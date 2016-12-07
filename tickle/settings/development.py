b21fbef0e6bf16ac09b45b4ca2dfa922574815ce
Olle Vidner <olle@vidner.se>
2016-12-07 14:32:29 +0100

    Added USE_X_FORWARDED_HOST with sane defaults for development and production, respectively

--- tickle/settings/development.py
+++ tickle/settings/development.py
@@ -8,6 +8,7 @@ SECRET_KEY = env.str('DJANGO_SECRET_KEY', 'dev')
 
 SESSION_COOKIE_SECURE = env.bool('DJANGO_SESSION_COOKIE_SECURE', False)
 CORS_ORIGIN_ALLOW_ALL = env.bool('DJANGO_CORS_ORIGIN_ALLOW_ALL', True)
+USE_X_FORWARDED_HOST = env.bool('DJANGO_USE_X_FORWARDED_HOST', False)
 
 # TESTING
 # ------------------------------------------------------------------------------
