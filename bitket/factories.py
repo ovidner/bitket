7fb2131a21bf079287ec6cb1d6747029d0679fff
Olle Vidner <olle@vidner.se>
2017-11-17 00:59:00 +0100

    Removed obsolete mixins

--- bitket/factories.py
+++ bitket/factories.py
@@ -4,24 +4,9 @@ from django.contrib.auth.hashers import make_password
 
 from . import models
 
-from .behaviors import NameSlugMixin, NameSlugDescriptionMixin
 
 
-class NameSlugFactoryMixin(factory.DjangoModelFactory):
-    name = factory.fuzzy.FuzzyText()
-    slug = factory.LazyAttribute(lambda o: slugify(o.name))
 
-    class Meta:
-        model = NameSlugMixin
-        abstract = True
-
-
-class NameSlugDescriptionFactoryMixin(NameSlugFactoryMixin):
-    description = factory.fuzzy.FuzzyText(length=64)
-
-    class Meta:
-        model = NameSlugDescriptionMixin
-        abstract = True
 
 
 class ConditionFactory(factory.DjangoModelFactory):
