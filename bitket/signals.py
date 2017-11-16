7206bdef67c67379dcba1d71e29888ccdf0910a8
Olle Vidner <olle@vidner.se>
2017-11-16 21:34:36 +0100

    Removed some cruft

--- bitket/signals.py
+++
@@ -1,7 +0,0 @@
-from django.dispatch import receiver
-from django.contrib.auth.signals import user_logged_in
-
-
-@receiver(user_logged_in)
-def create_default_cart(sender, request, user, **kwargs):
-    user.create_default_cart()
